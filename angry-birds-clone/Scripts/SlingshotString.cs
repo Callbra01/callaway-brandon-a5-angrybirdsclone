6fc66cdb58e5fe3d2ff2ed4f768a91383c3c9dd6
wightkid <103604852+wightkid@users.noreply.github.com>
2024-12-13 14:26:21 -0500

    Add slingshot point node references

--- angry-birds-clone/Scripts/SlingshotString.cs
+++ angry-birds-clone/Scripts/SlingshotString.cs
@@ -3,13 +3,20 @@ using System;
 
 public partial class SlingshotString : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+
+    Node2D rightArmPoint;
+    Node2D leftArmPoint;
+    Node2D slingPoint;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        rightArmPoint = GetNode<Node2D>("Right Arm");
+        leftArmPoint = GetNode<Node2D>("Left Arm");
+        slingPoint = GetNode<Node2D>("Pull Point");
     }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
